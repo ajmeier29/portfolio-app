ef9e5d6fb96c5a00199da008b93c22d86e4267b5
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-06-04 16:44:18 -0400

    Moved feature to its own component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
 
     return (
         <Flex
-            bg="#edf3f8"
+            bg="blue.500"
             _dark={{
                 bg: "gray.800",
             }}
@@ -29,100 +29,11 @@ export default function AboutPage() {
                 px={8}
                 py={20}
                 mx="auto"
+                borderRadius={20}
             >
-                {FeaturesList.map((items) => (
-                    <SimpleGrid
-                        alignItems="start"
-                        columns={{
-                            base: 1,
-                            md: 2,
-                        }}
-                        mb={24}
-                        spacingY={{
-                            base: 10,
-                            md: 32,
-                        }}
-                        spacingX={{
-                            base: 10,
-                            md: 24,
-                        }}
-                    >
-                        <Box>
-                            <chakra.h2
-                                mb={4}
-                                fontSize={{
-                                    base: "2xl",
-                                    md: "4xl",
-                                }}
-                                fontWeight="extrabold"
-                                letterSpacing="tight"
-                                textAlign={{
-                                    base: "center",
-                                    md: "left",
-                                }}
-                                color="gray.900"
-                                _dark={{
-                                    color: "gray.400",
-                                }}
-                                lineHeight={{
-                                    md: "shorter",
-                                }}
-                                textShadow="2px 0 currentcolor"
-                            >
-                                {items.heading}
-                            </chakra.h2>
-                            <chakra.p
-                                mb={5}
-                                textAlign={{
-                                    base: "center",
-                                    sm: "left",
-                                }}
-                                color="gray.600"
-                                _dark={{
-                                    color: "gray.400",
-                                }}
-                                fontSize={{
-                                    md: "lg",
-                                }}
-                            >
-                                {items.description}
-                            </chakra.p>
-                            <Button
-                                w={{
-                                    base: "full",
-                                    sm: "auto",
-                                }}
-                                size="lg"
-                                bg="gray.900"
-                                _dark={{
-                                    bg: "gray.700",
-                                }}
-                                _hover={{
-                                    bg: "gray.700",
-                                    _dark: {
-                                        bg: "gray.600",
-                                    },
-                                }}
-                                color="gray.100"
-                                as="a"
-                            >
-                                {items.buttonText}
-                            </Button>
-                        </Box>
-                        <Box
-                            w="full"
-                            h="full"
-                            py={48}
-                            bg="gray.200"
-                            _dark={{
-                                bg: "gray.700",
-                            }}
-                        ></Box>
-                    </SimpleGrid>
+                {FeaturesList.map((item) => (
+                    <Feature key={item.description} {...item} />
                 ))}
-
-
-
 
                 {/* <SimpleGrid
                     alignItems="center"
@@ -145,6 +56,100 @@ export default function AboutPage() {
 
             </Box>
         </Flex>
+    );
+}
+
+const Feature = (items: Features) => {
+    return (
+        <SimpleGrid
+            alignItems="start"
+            columns={{
+                base: 1,
+                md: 2,
+            }}
+            mb={24}
+            spacingY={{
+                base: 10,
+                md: 32,
+            }}
+            spacingX={{
+                base: 10,
+                md: 24,
+            }}
+        >
+            <Box>
+                <chakra.h2
+                    mb={4}
+                    fontSize={{
+                        base: "2xl",
+                        md: "4xl",
+                    }}
+                    fontWeight="extrabold"
+                    letterSpacing="tight"
+                    textAlign={{
+                        base: "center",
+                        md: "left",
+                    }}
+                    color="gray.900"
+                    _dark={{
+                        color: "gray.400",
+                    }}
+                    lineHeight={{
+                        md: "shorter",
+                    }}
+                    textShadow="2px 0 currentcolor"
+                >
+                    {items.heading}
+                </chakra.h2>
+                <chakra.p
+                    mb={5}
+                    textAlign={{
+                        base: "center",
+                        sm: "left",
+                    }}
+                    color="gray.600"
+                    _dark={{
+                        color: "gray.400",
+                    }}
+                    fontSize={{
+                        md: "lg",
+                    }}
+                >
+                    {items.description}
+                </chakra.p>
+                <Button
+                    w={{
+                        base: "full",
+                        sm: "auto",
+                    }}
+                    size="lg"
+                    bg="gray.900"
+                    _dark={{
+                        bg: "gray.700",
+                    }}
+                    _hover={{
+                        bg: "gray.700",
+                        _dark: {
+                            bg: "gray.600",
+                        },
+                    }}
+                    color="gray.100"
+                    as="a"
+                >
+                    {items.buttonText}
+                </Button>
+            </Box>
+            <Box
+                w="full"
+                h="full"
+                py={48}
+                bg="gray.200"
+                _dark={{
+                    bg: "gray.700",
+                }}
+                borderRadius={10}
+            ></Box>
+        </SimpleGrid>
     );
 }
 
