b8bdb63a7988009b71de9b148abdb61621e26e01
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-06-04 22:44:26 -0400

    Made features flip fop more dynamic

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { Flex, SimpleGrid, chakra, Button, Box } from "@chakra-ui/react";
+import { RiAlignItemLeftFill } from "react-icons/ri";
 
 
 
@@ -31,9 +32,17 @@ export default function AboutPage() {
                 mx="auto"
                 borderRadius={20}
             >
-                {FeaturesList.map((item: Features, index: number) => (
-                    (index%2 === 0) ? <LeftFeature key={item.description} {...item} /> : <RightFeature key={item.description} {...item} />
-                ))}
+                
+                {FeaturesList.map((item: Features, index: number)  => {
+
+                    if (index % 2 === 0) {
+                        item.isLeftSide = false;
+                        return <Feature key={item.heading} {...item} />
+                    } else {
+                        item.isLeftSide = true;
+                        return <Feature key={item.heading} {...item} />
+                    }
+                })}
 
                 {/* <SimpleGrid
                     alignItems="center"
@@ -59,7 +68,8 @@ export default function AboutPage() {
     );
 }
 
-const LeftFeature = (items: Features) => {
+const Feature = (items: Features) => {
+    const { isLeftSide } = items;
     return (
         <SimpleGrid
             alignItems="center"
@@ -77,7 +87,12 @@ const LeftFeature = (items: Features) => {
                 md: 24,
             }}
         >
-            <Box>
+            <Box
+                order={{
+                    base: (isLeftSide ? "initial" : ""),
+                    md: (isLeftSide ? 2 : 0),
+                }}
+            >
                 <chakra.h2
                     mb={4}
                     fontSize={{
@@ -152,109 +167,12 @@ const LeftFeature = (items: Features) => {
     );
 }
 
-const RightFeature = (items: Features) => {
-    return (
-        <SimpleGrid
-            alignItems="center"
-            columns={{
-                base: 1,
-                md: 2,
-            }}
-            flexDirection="column-reverse"
-            mb={24}
-            spacingY={{
-                base: 10,
-                md: 32,
-            }}
-            spacingX={{
-                base: 10,
-                md: 24,
-            }}
-        >
-            <Box
-                order={{
-                    base: "initial",
-                    md: 2,
-                }}
-            >
-                <chakra.h2
-                    mb={4}
-                    fontSize={{
-                        base: "2xl",
-                        md: "4xl",
-                    }}
-                    fontWeight="extrabold"
-                    letterSpacing="tight"
-                    textAlign={{
-                        base: "center",
-                        md: "left",
-                    }}
-                    color="gray.900"
-                    _dark={{
-                        color: "gray.400",
-                    }}
-                    lineHeight={{
-                        md: "shorter",
-                    }}
-                >
-                    {items.heading}
-                </chakra.h2>
-                <chakra.p
-                    mb={5}
-                    textAlign={{
-                        base: "center",
-                        sm: "left",
-                    }}
-                    color="gray.600"
-                    _dark={{
-                        color: "gray.400",
-                    }}
-                    fontSize={{
-                        md: "lg",
-                    }}
-                >
-                    {items.description}
-                </chakra.p>
-                <Button
-                    w={{
-                        base: "full",
-                        sm: "auto",
-                    }}
-                    size="lg"
-                    bg="gray.900"
-                    _dark={{
-                        bg: "gray.700",
-                    }}
-                    _hover={{
-                        bg: "gray.700",
-                        _dark: {
-                            bg: "gray.600",
-                        },
-                    }}
-                    color="gray.100"
-                    as="a"
-                >
-                    {items.buttonText}
-                </Button>
-            </Box>
-            <Box
-                w="full"
-                h="full"
-                py={48}
-                bg="gray.200"
-                _dark={{
-                    bg: "gray.700",
-                }}
-            ></Box>
-        </SimpleGrid>
-    );
-}
-
 interface Features {
     heading: string,
     description: string,
     buttonText: string,
-    buttonUrl: string
+    buttonUrl: string,
+    isLeftSide?: boolean
 }
 
 const FeaturesList: Array<Features> = [
@@ -262,12 +180,18 @@ const FeaturesList: Array<Features> = [
         heading: '3D Printing Since 2016',
         description: 'I have been obsessed with 3D Printing since the day I bought my first 3D printer back in 2016. I bought the Monoprice FDM Printer. Since then I have taught myself how to make some models, tinker, and just flat out print the nerdiest things I could find on Thingiverse.',
         buttonText: 'Learn More',
-        buttonUrl: '#'
+        buttonUrl: '#',
     },
     {
         heading: 'Etsy Seller Since 2012',
         description: 'After years of 3D printing I decided to make my printers work for me. I was fascinated by the idea that I could just buy a 3D printer and pay it off by selling my prints over time. I dont sell much in terms of numbers but all my printers are paid off from sales!',
         buttonText: 'My Etsy Shop',
-        buttonUrl: '#'
+        buttonUrl: '#', 
+    },
+    {
+        heading: 'Etsy Seller Since 2012',
+        description: 'After years of 3D printing I decided to make my printers work for me. I was fascinated by the idea that I could just buy a 3D printer and pay it off by selling my prints over time. I dont sell much in terms of numbers but all my printers are paid off from sales!',
+        buttonText: 'My Etsy Shop',
+        buttonUrl: '#',
     }
 ];
