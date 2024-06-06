969af81681ac987258e46b61c5dc72cd21efe161
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-06-06 00:46:50 -0400

    Fixed Etsy Seller year

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -165,7 +165,7 @@ const FeaturesList: Array<Features> = [
         buttonUrl: '#',
     },
     {
-        heading: 'Etsy Seller Since 2012',
+        heading: 'Etsy Seller Since 2021',
         description: 'After years of 3D printing I decided to make my printers work for me. I was fascinated by the idea that I could just buy a 3D printer and pay it off by selling my prints over time. I dont sell much in terms of numbers but all my printers are paid off from sales!',
         buttonText: 'My Etsy Shop',
         buttonUrl: 'https://www.etsy.com/shop/3DPrintsByAndy',
