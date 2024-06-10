324717690d1e7f940860fb7ab483fa1b9df16c2b
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-06-09 22:24:54 -0400

    Fixed Hero Image on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -131,8 +131,8 @@ import {
                 alt={'Hero Image'}
                 fit={'cover'}
                 // align={{base: 'bottom', md: '0px -110px'}}
-                height={'400px'}
-                width={'900px'}
+                height={{base: '100%', md: '400px'}}
+                width={{base: '100%', md: '900px'}}
                 src={
                   'https://lh3.googleusercontent.com/pw/AP1GczMt6s1dWbtQF7dQrjpDazFKEUd9dAA03uwK43jRfqVco75BF0pKSEE3WFc6GVp89wFVoBevu32Q7B7mTkvg_w-OpCC4B7tl0aDE75JOjBKwDQuRkIMs=w2400'
                 }
